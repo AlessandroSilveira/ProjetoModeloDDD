d771c946e5042d8fe063f13f367d86da3954daf0
alessandro.silveira <alessandro.silveira@concrete.com.br>
2017-04-07 17:31:08 -0300

    enviando teste

--- ProjetoModeloDDD.Mvc/Global.asax.cs
+++ ProjetoModeloDDD.Mvc/Global.asax.cs
@@ -18,6 +18,7 @@ namespace ProjetoModeloDDD.Mvc
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+
 			AutoMapperConfig.RegisterMapping();
 		}
 	}
